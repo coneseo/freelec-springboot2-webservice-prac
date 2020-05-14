0fa6aabebf8e84f61a25bbe5aae00ff1ea5cf6a1
coneseo <woon4754@gmail.com>
2020-05-14 23:54:12 +0900

    #index.mustache, IndexController 추가

---
+++ src/main/java/com/coneseod/book/springboot/web/IndexController.java
@@ -0,0 +1,12 @@
+package com.coneseod.book.springboot.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+public class IndexController {
+
+  @GetMapping("/")
+  public String index(){
+    return "index";
+  }
+
+}
